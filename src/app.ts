5289506a27626a89d2004148b1b1504f3da54f29
Naveed Ahmed <126868408+Naveedahmedtech@users.noreply.github.com>
2024-12-20 12:34:06 +0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -22,20 +22,20 @@ import { ROUTES } from "./constants/routePaths";
 export const createApp = () => {
   const app = express();
   const allowedOrigins = "*";
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-      credentials: true,
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: function (origin, callback) {
+  //       if (!origin || allowedOrigins.includes(origin)) {
+  //         callback(null, true);
+  //       } else {
+  //         callback(new Error("Not allowed by CORS"));
+  //       }
+  //     },
+  //     credentials: true,
+  //   })
+  // );
 
-  app.use(cors());
+  app.use(cors("*"));
   app.use(
     session({
       secret: "session-secret-key",
